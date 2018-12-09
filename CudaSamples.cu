937a07c20783f62f9e47ac07eda8674705514b21
4Rom1 <>
2018-12-09 20:32:03 +0100

    Global index showing

--- CudaSamples.cu
+++ CudaSamples.cu
@@ -2,20 +2,25 @@
 #include <cuda_runtime.h>
 #include "stdio.h"
 #include <cstdlib>
+#include "math.h"
+#include "CudaSamples.h"
+#define NWarps 32
 
 
 __global__ void HelloWorld()
 {
 	//2D Index of current thread
-	const int ThreadXIndex = threadIdx.x;
-	printf("Hi from thread number %d\n",ThreadXIndex);	
+        int index = threadIdx.x + blockDim.x * blockIdx.x;
+        int tId = threadIdx.x;
+        int BlockId=blockIdx.x;
+	printf("Hi from thread number %d, Block number %d, global index %d\n",tId,BlockId,index);	
 }
 void CallHelloWorld(int N)
 {
 //N Threads per block 
-	const dim3 block(N,1);
+	const dim3 block(min(float(NWarps),float(N)),1,1);
 //1 block per grid
-	const dim3 grid(1);
+	const dim3 grid(iDivUp(N,NWarps),1,1);
 //Kernel launch
         HelloWorld<<<grid,block>>>();
 }
--- CudaSamples.h
+++ CudaSamples.h
@@ -1,6 +1,8 @@
 #ifndef CudaSamples_H
 #define CudaSamples_H
 
+inline int iDivUp(int a, int b) { return (a % b != 0) ? (a / b + 1) : (a / b); }
+
 void CallHelloWorld(int N);
 
 #endif
