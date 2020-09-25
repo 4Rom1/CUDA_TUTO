6d3d158ec9a4eb6b3c8b587cdea3a7f5a3c0eff7
Warispreet Singh <3051765@login1.pri.kelvin2.alces.network>
2020-09-25 14:37:19 +0100

    Add error checking

--- CudaSamples.h
+++ CudaSamples.h
@@ -3,6 +3,10 @@
 #define NWarps 32
 #include <cuda_runtime.h>
 #include <vector>
+#include <exception> 
+#include <iostream>
+#include <stdexcept>
+	
 //
 
 inline int iDivUp(int a, int b) { return (a % b != 0) ? (a / b + 1) : (a / b); }
@@ -24,6 +28,23 @@ void *SumUpVoid(void *PtDim);
 void parallelSqrtExp(float *data, 
   std::vector<cudaStream_t> streams, int N, int num_streams);
 
+inline void gpu_assert(cudaError_t code, const char *file, int line, bool abort = true)
+{
+	if (code != cudaSuccess)
+	{
+		std::cerr << "Cuda failure: " << cudaGetErrorString(code) << " " << file << " " << line << '\n';
+
+		if (abort)
+		{
+			throw std::runtime_error("Cuda Error!");
+		}
+	}
+}
+
 #define TIME_DIFFS(t1, t2) t2.tv_usec - t1.tv_usec
+#define GPU_ERROR_CHECK(ans)                                                                                           \
+	{                                                                                                                  \
+		gpu_assert(ans, __FILE__, __LINE__);                                                                           \
+	}
 
 #endif
